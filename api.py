3dbd1c04bf38763bbcb32816328e7ecc7f9b6c75
lzdnico <596049368@qq.com>
2019-11-03 22:27:12 +0800

    更换为ip

--- api.py
+++ api.py
@@ -17,7 +17,7 @@ urllib3.disable_warnings()
 
 #全局变量定义
 aff = 'STC可用，注册地址：bilibili.stchk.cloud/auth/register?code=gzI5'
-apiip = 'api.niconewbeee.tk:8080'    #8080为main函数里的监听端口
+apiip = '185.238.248.145:10086'    #8080为main函数里的监听端口
 
 
 
